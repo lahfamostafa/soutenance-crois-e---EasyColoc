c1da21d0ea403af28fc7eed08793cac309cac159
lahfamostafa <lahfa.mostafa1@gmail.com>
2026-02-25 15:03:11 +0000

    feat(membership): ajout de la table pivot memberships avec gestion des rôles et des adhésions

---
+++ database/migrations/2026_02_25_123005_create_member_ships_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('member_ships', function (Blueprint $table) {
+            $table->id();
+            $table->enum('role',['membre','owner']);
+            $table->boolean('is_active')->default(true);
+            $table->decimal('balance');
+            $table->dateTime('joined_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('member_ships');
+    }
+};
