34defbeefc2a4cf982383bc133bf588b15104c02
lahfamostafa <lahfa.mostafa1@gmail.com>
2026-03-01 19:54:22 +0000

    Implémenter la logique d'ajout des dépenses avec mise à jour des balances

--- app/Http/Controllers/ExpenceController.php
+++ app/Http/Controllers/ExpenceController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Colocation;
 use App\Models\Expence;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ExpenceController extends Controller
 {
@@ -26,9 +29,52 @@ class ExpenceController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Colocation $colocation)
     {
-        //
+        $isMember = $colocation->memberShips()->where('user_id',Auth::id())->whereNull('left_at')->exists();
+        if (!$isMember) {
+            return back()->with('error',"Voun n'êtes pas membre actif de cette colocation.");
+        }
+
+        $data = $request->validate([
+            'title' => ['required','string','max:255'],
+            'amount' => ['required','numeric','min:0.01'],
+            'expence_date' => ['required','date'],
+            'payer_id' => ['required','integer','exists:users,id']
+        ]);
+
+        $payerIsMember = $colocation->memberShips()->where('user_id',$data['payer_id'])->whereNull('left_at')->exists();
+
+        if (!$payerIsMember) {
+            return back()->with('error',"Le payer choisi n'est pas un membre actif de cette colocation.");
+        }
+
+        DB::transaction(function () use ($data,$colocation){
+            $expence = Expence::create([
+                'colocation_id' => $colocation->id,
+                'payer_id' => $data['payer_id'],
+                'title' => $data['title'],
+                'amount' => $data['amount'],
+                'expence_date' => $data['expence_date'],
+            ]);
+
+            $members = $colocation->memberShips()->whereNull('left_at')->lockForUpdate()->get();
+
+            $count = $members->count();
+            if($count === 0)return;
+
+            $share = $expence->amount / $count;
+
+            foreach ($members as $m) {
+                if ((int)$m->user_id === (int)$data['payer_id']) {
+                    $m->balance = (float)$m->balance + ((float)$expence->amount - (float)$share);
+                }else {
+                    $m->balance = (float)$m->balance - (float)$share;
+                }
+                $m->save();
+            }
+        });
+        return back()->with('success',"Expense ajouté + balances mis à jour.");
     }
 
     /**
